020f4fb5e3c2f11177d493ba45889e2ff874b840
KEREMADALI <keremadalik@gmail.com>
2024-11-08 11:15:06 +0300

    Finish timeline js

--- js/adjustDropdown.js
+++ js/adjustDropdown.js
@@ -9,21 +9,30 @@ const app = Vue.createApp({
     },
     methods: {
         update(dropdownId){
-
-            console.log(dropdownId);
-
             switch(dropdownId){
                 case 'dropdown1':
                     this.dropdown1State = !this.dropdown1State;
+                    this.dropdown2State = false;
+                    this.dropdown3State = false;
+                    this.dropdown4State = false;
                     break;
                 case 'dropdown2':
                     this.dropdown2State = !this.dropdown2State;
+                    this.dropdown1State = false;
+                    this.dropdown3State = false;
+                    this.dropdown4State = false;
                     break;  
                 case  'dropdown3':
                     this.dropdown3State = !this.dropdown3State;
+                    this.dropdown1State = false;
+                    this.dropdown2State = false;
+                    this.dropdown4State = false;
                     break;
                 case 'dropdown4':
                     this.dropdown4State = !this.dropdown4State;
+                    this.dropdown1State = false;
+                    this.dropdown2State = false;
+                    this.dropdown3State = false;
                     break;
                 default:
                     console.log("Missing dropdown id: " + dropdownId);
--- js/alterTimeline.js
+++ js/alterTimeline.js
@@ -1,16 +1,43 @@
-function selectPin(id){
-    var pin = document.getElementById(id);
+const app = Vue.createApp({
+    data() {
+        return {
+            pin1Selected: false,
+            pin2Selected: false,
+            pin3Selected: true,
+            pin4Selected: false,
+            pin5Selected: false,
+        };
+    },
+    methods: {
+        selectPin(event) {
 
-    // Reset pins
-    Array.from(pin.parentElement.getElementsByClassName("pin"))
-        .forEach(pin =>{
-            pin.getElementsByTagName("img")[0].src = "../images/company/icons/unselected.svg";
-            var text = pin.getElementsByTagName("div")[0];
-            text.className = "not-selected";
-        });
+            let id = event.target.id;
+            
+            this.pin1Selected = false;
+            this.pin2Selected = false;
+            this.pin3Selected = false;
+            this.pin4Selected = false;
+            this.pin5Selected = false;
 
-    // Adjust selected pin
-    var text = pin.getElementsByTagName("div")[0];
-    text.className = "selected";
-    pin.getElementsByTagName("img")[0].src = "../images/company/icons/selected.svg";
-}
+            switch(id){
+                case 'pin1':
+                    this.pin1Selected = true;
+                    break;
+                case 'pin2':
+                    this.pin2Selected = true;
+                    break;
+                case 'pin3':
+                    this.pin3Selected = true;
+                    break;
+                case 'pin4':
+                    this.pin4Selected = true;
+                    break;
+                case 'pin5':
+                    this.pin5Selected = true;
+                    break;
+            }
+        }
+    }
+});
+
+app.mount("#timeline-section");
